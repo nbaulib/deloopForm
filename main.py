a19693860397e7de6ed8bca920220af10cebb0ed
nbaulib <69161557+nbaulib@users.noreply.github.com>
2026-04-21 12:11:17 -0400

    no dash on date + improved path function + runtime m

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import date
 import time
 
+start = time.time()
+
 driver = webdriver.Chrome(options=webdriver.ChromeOptions())
 wait = WebDriverWait(driver, 20)
 
@@ -13,59 +15,54 @@ driver.get(form_url)
 
 today = date.today()
     
-def e_clickable(role, text, type):
-    if type == "listbox":
-        xpath = (
-        f'//div[@role="listitem"]'
-        f'[.//span[text()="{text}"]]'
-        f'//div[@role="{type}"]'
-    )
-    else: 
-        xpath = (
+def get_xpath(label, input_type):
+    if input_type == "listbox":
+       return (
             f'//div[@role="listitem"]'
-            f'[.//span[text()="{text}"]]'
-            f'//input[@type="{type}"]'
+            f'[.//span[text()="{label}"]]'
+            f'//div[@role="listbox"]'
+        )
+    return (
+            f'//div[@role="listitem"]'
+            f'[.//span[text()="{label}"]]'
+            f'//input[not(@aria-hidden="true")]'
         )
         
-    if type == "date":
-        return wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
-    else:
-        return wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+def wait_clickable(xpath):
+    return wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
 
-name_field = e_clickable("listitem", "Name", "text")
+def wait_visible(xpath):
+    return wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+
+name_field = wait_clickable(get_xpath("Name", "text"))
+name_field.click()
 name_field.send_keys("Nico")
 
 # Date
-date_field = e_clickable("listitem", "Date", "date")
-date_field.send_keys(today.strftime("%m-%d-%Y"))
+date_field = wait_visible(get_xpath("Date", "text"))
+date_field.send_keys(today.strftime("%m%d%Y"))
 
 # School dropdown
-school_dropdown = e_clickable("listitem", "School", "listbox")
+school_dropdown = wait_clickable(get_xpath("School","listbox"))
 school_dropdown.click()
 
-option = wait.until(EC.element_to_be_clickable((
-    By.XPATH,
-    '//div[@role="option"]//span[text()="a"]'
-)))
+option = wait_clickable('//div[@role="option"]//span[text()="a"]')
 option.click()
 
-time.sleep(1)
+time.sleep(1) # does not work if i comment this out
 
 # Submit
-submit_button = wait.until(EC.element_to_be_clickable((
-    By.XPATH,
-    '//span[text()="Submit"]/ancestor::div[@role="button"]'
-)))
+submit_button = wait_clickable('//span[text()="Submit"]/ancestor::div[@role="button"]')
 submit_button.click()
 
 # Wait for confirmation page
-wait.until(EC.presence_of_element_located((
-    By.XPATH, '//*[contains(text(),"response has been recorded")]'
-)))
+wait_visible('//*[contains(text(),"response has been recorded")]')
 
 driver.close()
+end = time.time()
+
+runtime = end - start
+print(round(runtime,3), "seconds")
 
 # https://codewithtj.blogspot.com/2024/01/python-script-to-fill-google-form.html
 # https://www.geeksforgeeks.org/python/automatically-filling-multiple-responses-into-a-google-form-with-selenium-and-python/
-
-# make structured logging for debug
